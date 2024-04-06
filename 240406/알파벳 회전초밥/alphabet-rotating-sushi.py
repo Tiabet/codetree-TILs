947b9ff0a716a3a5405df1f8c0348b40d2098277
김정현 <89983434+Tiabet@users.noreply.github.com>
2024-04-06 21:03:18 +0900

    Title: 알파벳 회전초밥, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240406/알파벳 회전초밥/alphabet-rotating-sushi.py
@@ -0,0 +1,16 @@
+a = input()
+b = input()
+
+idx=0
+count=0
+
+while idx<len(b)-1:
+    locate = a.find(b[idx])
+    if a[locate:].find(b[idx+1]) == -1 :
+        idx+=1
+        count+=1
+    else :
+
+        idx+=1
+
+print(count+1)
