d27fcdae3987ac96f217f194c74d0947a6953aeb
김정현 <89983434+Tiabet@users.noreply.github.com>
2024-04-13 14:36:21 +0900

    Title: 코드트리 오마카세, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240413/코드트리 오마카세/codetree-omakase.py
@@ -0,0 +1,91 @@
+L = 0
+L, Q = map(int, input().split())
+
+Sushi_table = []
+customer_table = [[None,0] for _ in range(L)]
+
+t = 0
+
+def eat(Sushi_table, customer_table):
+    new_Sushi_table = [i for i in Sushi_table]
+    for i in range(L):
+        for j in range(len(Sushi_table)):
+            if customer_table[i][0] == Sushi_table[j][0] and customer_table[i][0]!=None:
+                if i == Sushi_table[j][1]:
+                    customer_table[i][1] -= 1
+                    new_Sushi_table.pop(j)
+
+    return new_Sushi_table, customer_table
+
+def turn_table(Sushi_table, turn):
+    for j in range(len(Sushi_table)):
+        Sushi_table[j][1] = (Sushi_table[j][1] + turn)%L
+
+    return Sushi_table
+
+
+for _ in range(Q):
+    inputs = list(map(str, input().split()))
+    
+    if int(inputs[0]) == 100 :
+        Time,x,name = int(inputs[1]), int(inputs[2]), inputs[3]
+
+        #테이블 돌아가면서 초밥 올려놓고 초밥 먹기
+        while t<Time :
+            Sushi_table = turn_table(Sushi_table, 1)
+            Sushi_table.append([name,x])
+            Sushi_table, customer_table = eat(Sushi_table, customer_table)
+            t+=1
+        
+        #다먹은 손님 퇴장
+        for i in range(L):
+            if customer_table[i][1] == 0 and customer_table[i][0] != None :
+                customer_table[i] = (None,0)
+
+        #print(Sushi_table)
+        #print(customer_table)
+
+    elif int(inputs[0]) == 200:
+        Time, x, name, n = int(inputs[1]), int(inputs[2]), inputs[3], int(inputs[4])
+
+        #테이블 돌아가면서 손님 앉히고 초밥 먹기
+        while t<Time :
+            Sushi_table = turn_table(Sushi_table, 1)
+            customer_table[x] = [name,n]
+            Sushi_table, customer_table = eat(Sushi_table, customer_table)
+            t+=1
+
+        #다먹은 손님 퇴장
+        for i in range(L):
+            if customer_table[i][1] == 0 and customer_table[i][0] != None :
+                customer_table[i] = [None,0]
+        #print(Sushi_table)
+        #print(customer_table)
+
+    elif int(inputs[0]) == 300 :
+        Time = int(inputs[1])
+
+        #테이블 돌아가면서 초밥 먹기
+        while t<Time :
+            Sushi_table = turn_table(Sushi_table, 1)
+            Sushi_table, customer_table = eat(Sushi_table, customer_table)
+            t+=1
+
+        #다먹은 손님 퇴장
+        for i in range(L):
+            if customer_table[i][1] == 0 and customer_table[i][0] != None :
+                customer_table[i] = [None,0]
+        
+        num_customer = 0
+        num_sushi = 0
+
+        for i in range(L):
+            if customer_table[i][0] != None :
+                num_customer+=1
+        
+        for j in range(len(Sushi_table)):
+            if Sushi_table[j][0] != None :
+                num_sushi+=1
+        #print(Sushi_table)
+        #print(customer_table)
+        print(num_customer, num_sushi)
