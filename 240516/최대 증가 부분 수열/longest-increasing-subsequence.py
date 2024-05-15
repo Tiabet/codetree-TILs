e44351e502e2561776e62f36590e35bc85c196ef
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-05-16 00:27:50 +0900

    Title: 최대 증가 부분 수열, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240516/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,32 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
+n = int(input())
+dp = [0] * n
+a = list(map(int, input().split()))
+
+def init():
+    for i in range(n):
+        dp[i] = INT_MIN
+    
+    dp[0] = 1
+
+
+init()
+
+for i in range(1,n):
+    for j in range(0,i):
+        if dp[j] == INT_MIN:
+            continue
+        if a[i]>a[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+        elif a[i] == a[j]:
+            dp[i] = dp[j]
+
+
+ans=0
+for i in range(n):
+    ans = max(ans, dp[i])
+
+print(ans)
