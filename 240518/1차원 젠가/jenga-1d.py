b576f3edd038f473aa2e450b9f58b7e040cc5c2a
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-05-18 00:26:38 +0900

    Title: 1차원 젠가, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/1차원 젠가/jenga-1d.py
+++ 240518/1차원 젠가/jenga-1d.py
@@ -13,16 +13,25 @@ s2 -= 1
 e1 -= 1
 e2 -= 1
 
-for i in range(2):
-    tmp = []
-    for i in range(s1,e1+1):
-        arr[i] = 0
+tmp = []
+for i in range(s1,e1+1):
+    arr[i] = 0
 
-    for i in range(n):
-        if arr[i] != 0 :
-            tmp.append(arr[i])
+for i in range(n):
+    if arr[i] != 0 :
+        tmp.append(arr[i])
 
-    arr = tmp
+arr = tmp
+tmp = []
+for i in range(s2,e2+1):
+    arr[i] = 0
 
+for i in range(len(arr)):
+    if arr[i] != 0 :
+        tmp.append(arr[i])
+
+arr = tmp
+
+print(len(arr))
 for i in range(len(arr)):
     print(arr[i])
