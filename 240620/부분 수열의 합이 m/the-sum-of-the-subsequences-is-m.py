e248967914775a0bca29385b05245369a825a87a
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-06-20 20:08:07 +0900

    Title: 부분 수열의 합이 m, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240620/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,20 @@
+import sys
+
+n,m = map(int, input().split())
+
+INT_MAX = sys.maxsize
+
+arr = list(map(int, input().split()))
+
+dp = [0] * (sum(arr)+1)
+for i in range(1,sum(arr)+1):
+    dp[i] = INT_MAX
+
+for i in range(n):
+    for j in range(sum(arr),-1,-1):
+        if j >=arr[i]:
+            if dp[j - arr[i]] == INT_MAX:
+                continue
+        dp[j] = min(dp[j],dp[j-arr[i]]+1)
+
+print(dp[m])
