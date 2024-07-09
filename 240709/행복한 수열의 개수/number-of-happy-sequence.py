9bb8fee6d5b3bc2161142b6af6f8163bdf0d20dc
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-07-09 23:18:01 +0900

    Title: 행복한 수열의 개수, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240709/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240709/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,44 +1,44 @@
-# Read n and m
 n, m = map(int, input().split())
 
-# Read the graph (n x n matrix)
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
 result = 0
 
-# Function to check consecutive elements in a list
-def count_consecutive(lst, m):
+for i in range(n):
+    tmp = graph[i]
     idx = 0
-    value = lst[0]
-    count = 0
-    while idx < len(lst) - 1:
-        if lst[idx] == lst[idx + 1]:
-            if count == 0:
-                value = lst[idx + 1]
-                count = 1
-            else:
-                if lst[idx] == value:
-                    count += 1
-        else:
-            count = 0
-            value = lst[idx + 1]
-        if count >= m - 1:
-            return True
-        idx += 1
-    return False
+    count=0
+    temp=0
+    while idx < n-1 :
+        if tmp[idx] == tmp[idx+1] and count==0:
+            # print('첫번째',i)
+            temp+=1
+            if temp>count:
+                count=temp
 
-# Check rows
+        idx+=1
+    if count >= m-1:
+        result+=1
+        
 for i in range(n):
-    if count_consecutive(graph[i], m):
-        result += 1
+    tmp = []
+    for j in range(n):
+        tmp.append(graph[j][i])
+    
+    idx = 0
+    count=0
+    temp=0
+    while idx < n-1 :
+        if tmp[idx] == tmp[idx+1] and count==0:
+            # print('첫번째',i)
+            temp+=1
+            if temp>count:
+                count=temp
 
-# Check columns
-for i in range(n):
-    column = [graph[j][i] for j in range(n)]
-    if count_consecutive(column, m):
-        result += 1
+        idx+=1
+    if count >= m-1:
+        result+=1
 
-# Print the result
 print(result)
