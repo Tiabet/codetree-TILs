b34c0ce452ffc223c7332877526ff0ccc206ebaa
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-07-13 22:26:40 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240713/그래프 탐색/graph-traversal.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+
+starts = []
+ends = []
+
+for _ in range(m):
+    start, end = map(int, input().split())
+    starts.append(starts)
+    ends.append(end)
+
+graph = [[] for _ in range(m+1)]
+
+for start, end in zip(starts,ends):
+    graph[start].append(end)
+    graph[end].append[start]
+    
+visited = [False for _ in range(n+1)]
+
+def dfs(point):
+    for cur_point in graph[point]:
+        if not visited[cur_point]:
+            visited[cur_point] = True
+            dfs(cur_point)
+
+visited[1] = True
+dfs(1)
+print(sum(visited)-1)
