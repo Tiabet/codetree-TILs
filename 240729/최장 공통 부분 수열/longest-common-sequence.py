1d2568a7102c4c0162d7cc5a3b03574e7760b321
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-07-29 15:10:08 +0900

    Title: 최장 공통 부분 수열, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240729/최장 공통 부분 수열/longest-common-sequence.py
+++ 240729/최장 공통 부분 수열/longest-common-sequence.py
@@ -6,20 +6,24 @@ len_b = len(B)
 
 dp = [[0 for _ in range(len_b)] for _ in range(len_a)]
 
-dp [0][0] = 1 if A[0]==B[0] else 0
+dp[0][0] = 1 if A[0]==B[0] else 0
 
-for i in range(len_a):
-    for j in range(len_b):
-        if (i == 0 and j !=0) or (i!=0 and j==0) :
-            if A[i] == B[j] :
-                dp[i][j] = 1
-            else :
-                dp[i][j] = 0
+for i in range(1,len_a):
+    if A[i]== B[0]:
+        for j in range(i,len_a):
+            dp[j][0] = 1
 
-        elif i !=0 and j !=0 :
-            if A[i] == B[j]:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + 1
-            else :
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+for i in range(1,len_b):
+    if B[i] == A[0] :
+        for j in range(i,len_b):
+            dp[0][j] = 1
 
-print(dp[len_a-1][len_b-1])
+for i in range(1,len_a):
+    for j in range(1,len_b):
+        if A[i] == B[j]:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + 1
+        else :
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp)
+print(dp[len_a-1][len_b-1]-1)
