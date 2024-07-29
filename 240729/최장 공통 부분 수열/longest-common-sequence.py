85846745e65217f08445dd1de98306342b5b5415
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-07-29 15:46:51 +0900

    Title: 최장 공통 부분 수열, Time: 161ms, Memory: 31MB, Status: Passed - CodeTree

--- 240729/최장 공통 부분 수열/longest-common-sequence.py
+++ 240729/최장 공통 부분 수열/longest-common-sequence.py
@@ -4,33 +4,26 @@ B = input()
 len_a = len(A)
 len_b = len(B)
 
+# Initialize the dp table
 dp = [[0 for _ in range(len_b)] for _ in range(len_a)]
 
-# dp[0][0] = 1 if A[0]==B[0] else 0
+# Initialize the first row and first column of the dp table
+if A[0] == B[0]:
+    dp[0][0] = 1
 
-if A[0] == B[0] :
-    dp[0] = [1 for _ in range(len_b)]
-    for i in range(len_a):
-        dp[i][0] = 1
+for i in range(1, len_a):
+    dp[i][0] = dp[i-1][0] if A[i] != B[0] else 1
 
-for i in range(1,len_a):
-    if A[i]== B[0]:
-        for j in range(i,len_a):
-            dp[j][0] = 1
+for j in range(1, len_b):
+    dp[0][j] = dp[0][j-1] if B[j] != A[0] else 1
 
-for i in range(1,len_b):
-    if B[i] == A[0] :
-        for j in range(i,len_b):
-            dp[0][j] = 1
-
-for i in range(1,len_a):
-    for j in range(1,len_b):
+# Fill the dp table
+for i in range(1, len_a):
+    for j in range(1, len_b):
         if A[i] == B[j]:
-            dp[i][j:len_b] = [(max(dp[i-1][j], dp[i][j-1]) + 1) for _ in range(j,len_b)]
-            break
-        else :
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-            
 
-# print(dp)
-print(max(dp[len_a-1]))
+# The length of the longest common subsequence is in the bottom-right cell of the dp table
+print(dp[len_a-1][len_b-1])
