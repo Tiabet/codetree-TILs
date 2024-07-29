2746bddad03bc66d61dcd2eb2490ebb27abb038e
JungHyoun Kim <89983434+Tiabet@users.noreply.github.com>
2024-07-29 14:56:49 +0900

    Title: 최장 공통 부분 수열, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240729/최장 공통 부분 수열/longest-common-sequence.py
+++ 240729/최장 공통 부분 수열/longest-common-sequence.py
@@ -4,12 +4,12 @@ B = input()
 len_a = len(A)
 len_b = len(B)
 
-dp = [[0 for _ in range(len_a)] for _ in range(len_b)]
+dp = [[0 for _ in range(len_b)] for _ in range(len_a)]
 
 dp [0][0] = 1 if A[0]==B[0] else 0
 
-for i in range(len_b):
-    for j in range(len_a):
+for i in range(len_a):
+    for j in range(len_b):
         if (i == 0 and j !=0) or (i!=0 and j==0) :
             if A[i] == B[j] :
                 dp[i][j] = 1
